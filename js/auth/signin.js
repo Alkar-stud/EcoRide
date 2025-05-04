5ba8d73b0ff370de958d08c16fd5e83b57c727ac
Alkar-stud <alexandre.kapps@gmail.com>
2025-05-04 09:30:49 +0200

    feat: Ajout de la validation du formulaire d'inscription et intégration de l'API pour l'enregistrement

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -7,39 +7,7 @@ const signinForm = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials);
 
-/*
-function checkCredentials(){
-    //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
-    
-    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
-        //Il faudra récupérer le vrai token
-        const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
-        setToken(token);
-        //placer ce token en cookie
-
-        setCookie(RoleCookieName, "client", 7);
-        //On cherche si on n'essaie pas d'atteindre une autre page, sinon page accueil
-        //On récupère les paramètres de la page courante
-        const urlParams = new URLSearchParams(window.location.search);
-        //Si le paramètre page existe, on redirige vers cette page
-        const redirectPage = urlParams.get("page");
-        if (redirectPage) {
-            window.location.replace(redirectPage);
-        } else {
-            window.location.replace("/");
-        }
-    }
-    else{
-        mailInput.classList.add("is-invalid");
-        passwordInput.classList.add("is-invalid");
-    }
-}
-*/
-/*
- * La fonction ci-dessus est quand on simule une connexion avec un utilisateur fictif
- * La fonction ci-desous est quand on appelle l'API
- */
-
+//Function permettant de valider tout le formulaire
 function checkCredentials(){
     let dataForm = new FormData(signinForm);
     
