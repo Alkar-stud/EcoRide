5c6505661b78268d55cfa23889c3cfd946a679e5
Alkar-stud <alexandre.kapps@gmail.com>
2025-05-09 18:29:14 +0200

    Commit all current changes

--- js/account/account.js
+++ js/account/account.js
@@ -2,8 +2,6 @@ import { getUserInfo, setUserInfo, deleteAccount, checkRoleSelection, validateFo
 import { addPreferences, savePreference, deletePreference } from './account-preferences.js';
 import { addVehicle, loadEnergyOptions } from './account-vehicles.js';
 
-alert('ok');
-
 //Pour les infos perso du user
 const pseudoInput = document.getElementById("PseudoInput");
 const photo = document.getElementById("photo"); // Affichage de la photo
